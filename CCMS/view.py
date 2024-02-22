06fd518d2ab71971e8b342f025d232e226a0c702
= <=>
2024-02-22 13:51:48 +0800

    content

--- CCMS/view.py
+++ CCMS/view.py
@@ -4,21 +4,40 @@ def index(request):
     return HttpResponse("Hello, World!")
 
 def mission(request):
-    msg="The College of Computing and Multimedia Studies shall produce competent and \
+    msg="<h1>MISSION<h1/>\
+    \
+        <p>The College of Computing and Multimedia Studies shall produce competent and \
         innovative professionals or Technopreneurs in the Information and Communication \
             Technology (ICT) industry adequately prepared in the practice of their profession\
                   supportive of national development goals and standards of global excellence.\
-    <h1/>"
+    <p/>"
     return HttpResponse(msg)
 
 def vission(request):
-    msg="<h1>Name: Jethro D. Labitigan<br>\
-        Email: jetjetlabitigan@gmail.com\
-    <h1/>"
+    msg="<h1>VISSION<h1/>\
+        \
+        <p>The College of Computing and Multimedia Studies shall be a center of excellence in \
+        delivering Computing and Multimedia education.\
+    <p/>"
     return HttpResponse(msg)
 
 def objectives(request):
-    msg="<h1>Name: Jethro D. Labitigan<br>\
-        Email: jetjetlabitigan@gmail.com\
-    <h1/>"
+    msg="<h1>VISSION<h1/>\
+        \
+<p>1. Increase faculty performance by obtaining a minimum rating of 4.00 in the semestral faculty evaluation of each faculty for the next fiveyears.\
+\
+2. Maintain competent faculty line-up by sending all permanent fulltime faculty to at least one (1) IT related training, conference, \
+orseminar annually for the next five years.\
+\
+3. Conduct a minimum of two (2) researches, IT projects or production of instructional materials annually for the next five years.\
+Achieve a minimum of 50% student passing percentage in the IT certification annually for the next five years.\
+\
+4. Maintain state-of-the-art information technology learning environment through annual procurement or upgrading of \
+hardwareor software licenses for at least one computer laboratory for the next five years.\
+\
+5. Set up minimum of two (2) academe-industry partnership projects and commercialization initiatives or research publication \
+andpresentation annually in preparation for the next CHED COD/COE application.\
+\
+6. Strengthen promotion program to increase freshman enrollees to a minimum of three (3) class sections annually for the next five years.\
+    <p/>"
     return HttpResponse(msg)
